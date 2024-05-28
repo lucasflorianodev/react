d757b7c279d039ac8c60207ea1d3b5f440c965a8
lucasflorianodev <lucasfloriano09@gmail.com>
2024-05-28 16:30:40 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import GerencimentoAlunos from './components/GerencimentoAlunos'
+import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
+import Aluno from './components/Aluno'
 
 const App = () => {
   return (
-    <div><GerencimentoAlunos /></div>
+    //<div><EstouConseguindoAprenderReact estouConseguindo = {false}/></div>
+    <div><EstouConseguindoAprenderReact estouConseguindo = {false}/>{
+      [
+      {nome: "Emilly", email: "esmi@gmail.com", curso: "Sistemas para internet", media: 9},
+      {nome: "Nattan", email: "nattanzinho@gmail.com", curso: "Sistemas para internet", media: 6},
+      {nome: "Claudio", email: "claudinho@gmail.com", curso: "Sistemas da informação", media: 7}
+      ].map((aluno) => <Aluno nome = {aluno.nome} email = {aluno.email} curso = {aluno.curso} media = {aluno.media}/>)
+    }</div>
+    
   )
 }
 
