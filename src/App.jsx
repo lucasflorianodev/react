16e8989062c25881b84da1c353bdb78905db8043
lucasflorianodev <lucasfloriano09@gmail.com>
2024-05-29 07:20:07 -0300

    Aula6

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
-import EstouConseguindoAprenderReact from './components/EstouConseguindoAprenderReact'
-import Aluno from './components/Aluno'
+import Inicial from './pages/Inicial'
+import Afaculdade from './pages/Afaculdade'
+import DpoLgpd from './pages/DpoLgpd'
+import NoticiasAxios from './pages/NoticiasAxios'
+import VisualizaNoticia from './pages/VisualizaNoticia'
+import AdminNoticias from './pages/admin/AdminNoticias'
+import CadastroNoticia from './pages/admin/CadastroNoticia'
+import EditarNoticia from './pages/admin/EditarNoticia'
+import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Container, Box } from '@mui/material'
 
 const App = () => {
   return (
-    //<div><EstouConseguindoAprenderReact estouConseguindo = {false}/></div>
-    <div><EstouConseguindoAprenderReact estouConseguindo = {false}/>{
-      [
-      {nome: "Emilly", email: "esmi@gmail.com", curso: "Sistemas para internet", media: 9},
-      {nome: "Nattan", email: "nattanzinho@gmail.com", curso: "Sistemas para internet", media: 6},
-      {nome: "Claudio", email: "claudinho@gmail.com", curso: "Sistemas da informação", media: 7}
-      ].map((aluno) => <Aluno nome = {aluno.nome} email = {aluno.email} curso = {aluno.curso} media = {aluno.media}/>)
-    }</div>
-    
+    <BrowserRouter>
+      <Container>
+        <Box display="flex" alignItems="center" justifyContent="space-between" p={2}>
+          <img src='uniesp.jpg' width={100} height={100} alt="Uniesp Logo" />
+          <Navbar />
+        </Box>
+        <Box mt={4}>
+          <Routes>
+            <Route path='/' element={<Inicial />} />
+            <Route path='/a-faculdade' element={<Afaculdade />} />
+            <Route path='/dpo-lgpd' element={<DpoLgpd />} />
+            <Route path='/noticias' element={<NoticiasAxios />} />
+            <Route path='/visualiza-noticia/:id' element={<VisualizaNoticia />} />
+            <Route path="/admin-noticias" element={<AdminNoticias />} />
+            <Route path="/cadastrar-noticia" element={<CadastroNoticia />} />
+            <Route path="/editar-noticia/:id" element={<EditarNoticia />} />
+          </Routes>
+        </Box>
+      </Container>
+    </BrowserRouter>
   )
 }
 
